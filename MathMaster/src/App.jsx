4cdb1546c86640e065a81eb94d35e74ebf790d0b
unknown <santiagocornue.p.e.t20@gmail.com>
2023-11-05 23:59:51 -0300

    9 botones

--- MathMaster/src/App.jsx
+++ MathMaster/src/App.jsx
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <>
+   <Boton simbolo="1"></Boton>
    <Boton simbolo="2"></Boton>
+   <Boton simbolo="3"></Boton>
+   <Boton simbolo="4"></Boton>
+   <Boton simbolo="5"></Boton>
+   <Boton simbolo="6"></Boton>
+   <Boton simbolo="7"></Boton>
+   <Boton simbolo="8"></Boton>
+   <Boton simbolo="9"></Boton>
     </>
   )
 }
