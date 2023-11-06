a041c68b9e8bb9f24796a0dd1f7a10d73271b015
unknown <santiagocornue.p.e.t20@gmail.com>
2023-11-05 23:58:20 -0300

    kamel descarga esto pedazo de petero

--- MathMaster/src/App.jsx
+++ MathMaster/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react"
-
+import Boton from "./components/Boton/Boton"
 
 function App() {
   
 
   return (
     <>
-
+   <Boton simbolo="2"></Boton>
     </>
   )
 }
---
+++ MathMaster/src/components/Boton/Boton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Boton = ({simbolo}) => {
+  
+  return (
+    <>
+    <button>{simbolo}</button>
+    </>
+  )
+}
+
+export default Boton
