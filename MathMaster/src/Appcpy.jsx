0ec54a63e6106db5ec6b8b289cd475a8933744c3
kamel dayoub <kamel.dayoub.epet20@gmail.com>
2023-11-14 23:24:27 -0300

    She do whatever she like And that just don't seem right Yeah, make people so mad Yeah, and want her so bad, hm

--- MathMaster/src/Appcpy.jsx
+++ MathMaster/src/Appcpy.jsx
@@ -3,59 +3,38 @@ import * as math from 'mathjs';
 import "./etyle.css"
 function App() {
   const [inputValue, setInputValue] = useState('');
-  const [result, setResult] = useState('');
-  const [history, setHistory] = useState([]); // Nuevo estado para el historial
-  const [showHistory, setShowHistory] = useState(false); // Nuevo estado para controlar la visualización del historial
-
-  const toggleHistory = () => {
-    setShowHistory(!showHistory); // Cambiar el estado de showHistory cada vez que se haga clic en el botón
-  };
   const handleNumberClick = (num) => {
-    const lastChar = inputValue[inputValue.length - 1];
-    if ('+-*/^%'.includes(lastChar) && '+-*/^%'.includes(num)) {
-      return; // No hacer nada si el último carácter y el nuevo carácter son signos
-    }
     setInputValue(inputValue + num);
   };
-  
+
   const clearInput = () => {
     setInputValue('');
-    setResult('');
-  };
-  
-  const deleteLastChar = () => {
-    setInputValue(inputValue.slice(0, -1)); // Eliminar el último carácter de inputValue
   };
 
   const calculateResult = () => {
     try {
+      // Replace the square root symbol with 'sqrt' before calculation
       const input = inputValue.replace(/√/g, 'sqrt');
-      const result = math.evaluate(input).toString();
-      setInputValue(result); // Actualizar inputValue en lugar de result
-      const newHistory = [...history, `${input} = ${result}`]; // Agregar al historial
-      if (newHistory.length > 5) {
-        newHistory.shift(); // Eliminar el cálculo más antiguo si el historial tiene más de 5 elementos
-      }
-      setHistory(newHistory);
+      setInputValue(math.evaluate(input).toString());
     } catch (error) {
-      setInputValue(error.message); // Actualizar inputValue en lugar de result
+      setInputValue(error.message);
     }
   };
 
   const addSqrt = () => {
+    // Add the square root symbol to the input
     setInputValue(`√(${inputValue})`);
   };
 
   return (
     <div className="App">
-      {/* <h1>Math Master</h1> */}
+      <h1>Math Master</h1>
       <div className="calculator">
         <input className='testo' type="text" value={inputValue} readOnly />
         <div className="buttons">
           <div className="row">
-          <button className='histo' onClick={toggleHistory}>historial</button>
+            <button className='histo' onClick={() => handleNumberClick('')}>histotial</button>
             <button className='butonAC' onClick={clearInput}>AC</button>
-            <button className='butonAC' onClick={deleteLastChar}>DEL</button> {/* Nuevo botón para borrar el último carácter */}
           </div>
           <div className="row">
             <button className='buton1' onClick={() => handleNumberClick('%')}>%</button>
@@ -88,18 +67,7 @@ function App() {
             <button className='square-button' onClick={calculateResult}>=</button>
             <button className='buton1' onClick={() => handleNumberClick('*')}>*</button>
           </div>
-          {/* <p>Resultado: {result}</p> */}
-          {showHistory && ( // Mostrar el historial solo si showHistory es true
-            <div className="history">
-              <h2>Historial de cálculos:</h2>
-              <ul>
-                {history.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-
+          
         </div>
       </div>
     </div>
